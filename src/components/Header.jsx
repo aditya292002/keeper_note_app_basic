f16f1582d7bcb8ea6aadd220cbce6b64f83cfbc0
Aditya <keshariaditya90@gmail.com>
2024-03-21 21:01:04 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LightbulbIcon from '@mui/icons-material/Lightbulb';
+
+function Header() {
+  return (
+    <header>
+      <h1 style={{fontFamily: "Montserrat Alternates",  fontWeight: "500", fontStyle:"regular"}}> <LightbulbIcon /> Keeper</h1>
+    </header>
+  );
+}
+
+export default Header;
